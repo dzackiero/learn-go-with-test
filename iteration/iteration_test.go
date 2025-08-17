abd2979fdb574ef6e2722db17b47d5bb06da56f8
dzackiero <dzakynashshar@gmai.com>
2025-08-17 18:35:03 +0700

    writing a repeat function to learn iteration in go

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -3,10 +3,18 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	got := Repeat("a")
-	want := "aaaaa"
+	t.Run("Repeat a character with how many times character repeated", func(t *testing.T) {
+		got := Repeat("a", 5)
+		want := "aaaaa"
 
-	if got != want {
-		t.Errorf("expected '%s' but got '%s'", want, got)
+		if got != want {
+			t.Errorf("expected '%s' but got '%s'", want, got)
+		}
+	})
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for b.Loop() {
+		Repeat("a", 5)
 	}
 }
